298bf19f616e2663a6bcb6b697d1699e53d9e676
Quénard <vincent.quenard@free.fr>
2021-08-19 17:55:41 +0200

    échec mise place trigger, début construction modal

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+/*idée fonction apparition menu au scroll de la souris
+function display_scroll() {
+    const headerScroll = document.querySelector('.header_scroll');
+    window.addEventListener('scroll', ()=>{
+        if (window.scrollY > 40) {
+            headerScroll.style.display = 'block';
+            headerScroll.style.opacity = '1';
+        }else{
+            headerScroll.style.display = 'none';
+            headerScroll.style.opacity = '0';
+        }
+    });
+}
+display_scroll;*/
+    
